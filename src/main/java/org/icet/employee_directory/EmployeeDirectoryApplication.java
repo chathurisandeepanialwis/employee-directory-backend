b9256e40caddc4590c07c58a1e84473efdbcf901
Chathuri  Sandeepani Alwis <chathurialwis@gmail.com>
2025-04-29 10:03:10 +0530

    project initialization

---
+++ src/main/java/org/icet/employee_directory/EmployeeDirectoryApplication.java
@@ -0,0 +1,13 @@
+package org.icet.employee_directory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeDirectoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeDirectoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/icet/employee_directory/EmployeeDirectoryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.icet.employee_directory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeDirectoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
